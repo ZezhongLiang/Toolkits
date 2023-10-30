95bb716f6483e6bfec0878e8b7cf8d17fe7af804
Zezhong Liang <68961917+ZezhongLiang@users.noreply.github.com>
2023-10-30 14:45:32 +0800

    Update, 30. Okt 2023

--- solve_mat.py
+++ solve_mat.py
@@ -7,16 +7,17 @@
                   Für den König, zu dem Licht!
 
 solve_mat.py
-This *.py file provides functions to solve matrix with greedy method.
+This *.py file provides functions to solve matrix.
 
-@ Last updates: 2. Okt 2023
+@ Last updates: 30. Okt 2023
 @ To-do: ok.
 """
 
 import itertools
 import os
 import sys
-from typing import Any
+import warnings
+from typing import Any, Literal
 
 import numpy as np
 import tqdm
@@ -35,11 +36,19 @@ except ImportError as _:
 # User preference
 WIDTH = 10
 HEIGHT = 16
+TARGET_SUM = 10
+FIGSIZE = (2.5, 5.0)
+
+# Decide method to use
+METHOD: Literal['gd', 'it'] = 'gd'
+
+# If greedy method is to be used
 WT_BORDER = 30
 S_BORDER = 2
-TARGET_SUM = 10
 MAX_SIZE = 15
-FIGSIZE = (2.5, 5.0)
+
+# If iterative method is to be used
+N_TIER = 0
 
 
 def pad_matrix(
@@ -201,7 +210,7 @@ def solve_matrix_single(mat: np.ndarray) -> np.ndarray:
 
             _ids_overlap = _ids_overlap[:MAX_SIZE]
 
-        for _j in tqdm.tqdm(range(1, 2 ** len(_ids_overlap), 1)):
+        for _j in range(1, 2 ** len(_ids_overlap), 1):
             _mats_sub_this = list()
             _ids_include = list()
 
@@ -232,6 +241,123 @@ def solve_matrix_single(mat: np.ndarray) -> np.ndarray:
 
     summary = summary.astype(np.float_)
     summary[summary == 0.0] = np.nan
+
+    return summary
+
+
+def _get_sub_matrixes(mat: np.ndarray) -> list[np.ndarray]:
+    """
+    _get_sub_matrixes function get all possible sub-matrixes.
+
+    Args:
+        mat (np.ndarray): matrix.
+
+    Returns:
+        list[np.ndarray]: all sub-matrixes possible.
+    """
+
+    sub_mats = list()
+
+    for (_i, _j) in itertools.product(range(HEIGHT), range(WIDTH)):
+
+        for _k in range(1, HEIGHT - _i + 1):
+
+            for _l in range(1, WIDTH - _j + 1):
+
+                sub_sum = np.sum(mat[_i : _i + _k, _j : _j + _l])
+
+                if sub_sum == TARGET_SUM:
+
+                    _sub_mat = np.full(
+                        (HEIGHT, WIDTH),
+                        dtype=np.bool_,
+                        fill_value=np.False_,
+                    )
+                    _sub_mat[_i : _i + _k, _j : _j + _l] = np.True_
+
+                    # Pad for display purposes
+                    _sub_mat[mat == 0] = np.False_
+
+                    for _t in sub_mats:
+                        if np.all(_t == _sub_mat):
+                            break
+                    else:
+                        sub_mats.append(_sub_mat)
+
+    return sub_mats
+
+
+def _score_matrix_iter(mat: np.ndarray, tier: int = N_TIER) -> int:
+    """
+    _score_matrix_iter function scores matrix by its maximal number of
+    possible solutions at a given tier.
+
+    Args:
+        mat (np.ndarray): matrix.
+        tier (int, optional): number of tier. Defaults to N_TIER.
+
+    Returns:
+        int: score.
+    """
+
+    if tier == 0:
+        _summary = solve_matrix_single(mat)
+
+        return np.sum((_summary * mat) > 0)
+    else:
+
+        sub_mats = _get_sub_matrixes(mat)
+        score = 0
+
+        for _sub_mat in sub_mats:
+            # Assume current sub-matrix has been taken out
+            _mat = mat.copy()
+            _mat[_sub_mat] = 0
+
+            # Iterate
+            _score = _score_matrix_iter(_mat, abs(tier - 1))
+            _score += np.sum(_sub_mat)
+            score = max(score, _score)
+
+    return score
+
+
+def solve_mat_iter(mat: np.ndarray, tier: int = 0) -> np.ndarray:
+    """
+    solve_mat_iter functin solves a matrix puzzle by evaluating the
+    score of possible solutions iteratively.
+
+    Args:
+        mat (np.ndarray): matrix puzzle.
+        tier (int, optional): number of tier. Defaults to 0.
+
+    Returns:
+        np.ndarray: summary of solution.
+    """
+
+    i_step = 1
+    summary = np.zeros_like(mat, dtype=np.int_)
+
+    while True:
+        (_id_best, _score_best) = (None, -1)
+        _sub_mats = _get_sub_matrixes(mat)
+
+        for (_i, _sub_mat) in enumerate(tqdm.tqdm(_sub_mats)):
+            _mat = mat.copy()
+            _mat[_sub_mat] = 0
+
+            _score = _score_matrix_iter(_mat, tier=tier)
+            _score *= np.sum(_sub_mat)
+
+            if _score > _score_best:
+                _id_best = _i
+
+        if _id_best is None:
+            break
+        else:
+            summary[summary == 0] = i_step * _sub_mat[summary == 0]
+            i_step += 1
+            mat = _mat.copy()
 
     return summary
 
@@ -287,22 +413,42 @@ if __name__ == '__main__':
     my_mat = np.loadtxt(filename, dtype=np.int64, delimiter=',')
     my_mat = my_mat.reshape((HEIGHT, WIDTH))
 
-    for _i in itertools.count():
+    if METHOD == 'gd':
 
-        print('__main__: step, {}.'.format(_i + 1))
+        for _i in itertools.count():
 
-        summary = solve_matrix_single(my_mat)
+            print('__main__: step, {}.'.format(_i + 1))
+
+            summary = solve_matrix_single(my_mat)
+            plot_figure(my_mat, summary)
+
+            if not np.nansum(summary):
+                break
+            else:
+                my_mat[summary > 0.0] = 0
+
+            plt.tight_layout()
+            plt.title(
+                'Step, {}, Score, {}'.format(_i + 1, np.sum(my_mat == 0.0))
+            )
+
+            plt.show()
+    elif METHOD == 'it':
+
+        summary = solve_mat_iter(my_mat, tier=N_TIER)
+        summary = summary.astype(np.float_)
+        summary[summary == 0.0] = np.nan
         plot_figure(my_mat, summary)
 
-        if not np.nansum(summary):
-            break
-        else:
-            my_mat[summary > 0.0] = 0
-
         plt.tight_layout()
-        plt.title('Step, {}, Score, {}'.format(_i + 1, np.sum(my_mat == 0.0)))
+        plt.title('Score, {}'.format(np.sum(np.isfinite(summary))))
 
         plt.show()
+    else:
+
+        warnings.warn(
+            '__main__: invalid method specification, {}.'.format(METHOD)
+        )
 
     print('__main__: cannot think of more solutions.')
 
